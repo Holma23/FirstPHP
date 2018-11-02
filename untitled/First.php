8340319bb113c91d67e3bbe35b2067e285bd3c16
Holma23 <44214084+Holma23@users.noreply.github.com>
2018-11-02 15:31:34 +0100

    Add files via upload

---
+++ untitled/First.php
@@ -0,0 +1,6 @@
+<?php
+echo'Ceci est une ligne créée uniquement en PHP <br>';
+echo'Ceci est la 2ème phrase créée avec PHP <br>';
+echo "<a href=\"https://gomycode.tn\">le site de GOMYCODE </a>";
+
+?>
